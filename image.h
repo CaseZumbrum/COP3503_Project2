603ddbc8ac4561a42060fe664fa42e81ee3cfd94
Case Zumbrum <casezumbrum@gmail.com>
2024-03-06 16:49:39 -0500

    created image class

--- image.h
+++ image.h
@@ -7,7 +7,27 @@
 
 
 class image {
-
+    struct Header {
+        char idLength;
+        char colorMapType;
+        char dataTypeCode;
+        short colorMapOrigin;
+        short colorMapLength;
+        char colorMapDepth;
+        short xOrigin;
+        short yOrigin;
+        short width;
+        short height;
+        char bitsPerPixel;
+        char imageDescriptor;
+        };
+    struct Color{
+        int blue;
+        int green;
+        int red;
+    };
+    Header header;
+    Color* arr;
 };
 
 
